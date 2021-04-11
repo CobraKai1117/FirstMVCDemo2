823b34aac81cb14657b810125d02707c3f97ff84
CobraKai1117 <kennethrpaulsen@gmail.com>
2021-04-11 01:01:39 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVCDemo2.Models;
+
+namespace FirstMVCDemo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(int id)
+        {
+            EmployeeBusinessLayer employeeBL = new EmployeeBusinessLayer();
+            Employee employee = employeeBL.GetEmployeeDetails(id);
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstMVCDemo2.MvcApplication" Language="C#" %>
